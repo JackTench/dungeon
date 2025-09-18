10837f6f09a56eefe70e02f865c56ac6fd7c8180
JackTench <79285604+JackTench@users.noreply.github.com>
2025-09-18 21:26:50 +0100

    Write constructor for board.

--- src/lib/board.ts
+++ src/lib/board.ts
@@ -1,4 +1,11 @@
-import type { CellType } from "./cell";
+import { CellType } from "./cell";
 
 // A board is a 2D array of cells.
 export type Board = CellType[][];
+
+// Constructor for board.
+export function createBoard(size: number): Board {
+  return Array.from({ length: size }, () =>
+        Array.from({ length: size }, () => CellType.Default)
+  );
+}
