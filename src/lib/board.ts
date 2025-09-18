b71d9144aa50e962f16b1c40574d4884ed2c83f0
JackTench <79285604+JackTench@users.noreply.github.com>
2025-09-18 21:23:33 +0100

    Define cell and board.

---
+++ src/lib/board.ts
@@ -0,0 +1,4 @@
+import type { CellType } from "./cell";
+
+// A board is a 2D array of cells.
+export type Board = CellType[][];
---
+++ src/lib/cell.ts
@@ -0,0 +1,8 @@
+// Type for a "cell" on the dungeon board.
+// TODO: Implement Chests and Enemies.
+export enum CellType {
+  Default,
+  Walkable,
+  Entrance,
+  Exit,
+}
