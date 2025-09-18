b9f3327537eb0c19d21d2b27104714d8518aa5a1
JackTench <79285604+JackTench@users.noreply.github.com>
2025-09-18 21:31:42 +0100

    Get color from cell.

--- src/lib/cell.ts
+++ src/lib/cell.ts
@@ -6,3 +6,24 @@ export enum CellType {
   Entrance,
   Exit,
 }
+
+// Get #ffffff color type for a given cell.
+export function getCellColor(cell: CellType): string {
+  switch(cell) {
+    // Default cells are black.
+    case CellType.Default:
+      return "#000000";
+    // Walkable cells are white.
+    case CellType.Walkable:
+      return "#ffffff";
+    // Entrances are green.
+    case CellType.Entrance:
+      return "#00aa00";
+    // Exits are gold.
+    case CellType.Exit:
+      return "#d4af37";
+
+    default:
+      return "#000000";
+  }
+}
